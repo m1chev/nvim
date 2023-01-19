3fb496a335a300276e2832a6b929d46ec6c60021
michev <georgi.michev@vereign.com>
2023-01-19 22:42:28 +0200

    Add fugitive and telescope key-maps
    Signed-off-by: michev <georgi.michev@vereign.com>

--- init.lua
+++ init.lua
@@ -278,6 +278,8 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sc', require('telescope.builtin').commands, { desc = '[S]earch [C]ommands' })
+vim.keymap.set('n', '<leader>st', require('telescope.builtin').colorscheme, { desc = '[S]earch [T]heme' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
