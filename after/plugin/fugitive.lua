a3ea9c07cb31a525c885a941c6a358242b5a5c93
michev <georgi.michev@vereign.com>
2023-01-20 11:54:23 +0200

    Signed commit #2

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -7,4 +7,3 @@ vim.keymap.set('n', '<leader>gw', ":GBrowse<CR>")
 vim.keymap.set('n', '<leader>gx', ":Gvdiffsplit!<CR>")
 vim.keymap.set('n', '<leader>gj', ":diffget //3<CR>") -- d3o
 vim.keymap.set('n', '<leader>gf', ":diffget //2<CR>") -- d2o
-
