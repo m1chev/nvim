d8ac11ab5d050906ccc9bc507d04e749671254de
michev <veselinov.michev@gmail.com>
2023-01-24 17:38:32 +0200

    Add new command in fugitive
    This command is used often,
    this is the reason to have in on shortcut.

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,9 +1,12 @@
+
 vim.keymap.set('n', '<leader>gs', ":G<CR>")
 vim.keymap.set('n', '<leader>gl', ":G log<CR>")
 vim.keymap.set('n', '<leader>gb', ":G blame<CR>")
 vim.keymap.set('n', '<leader>gd', ":G diff<CR>")
-vim.keymap.set('n', '<leader>gc', ":G commit -Sa<CR>")
+vim.keymap.set('n', '<leader>gc', ":G commit -S<CR>")
+vim.keymap.set('n', '<leader>gn', ":G checkout -b ")
 vim.keymap.set('n', '<leader>gw', ":GBrowse<CR>")
 vim.keymap.set('n', '<leader>gx', ":Gvdiffsplit!<CR>")
 vim.keymap.set('n', '<leader>gj', ":diffget //3<CR>") -- d3o
 vim.keymap.set('n', '<leader>gf', ":diffget //2<CR>") -- d2o
+
